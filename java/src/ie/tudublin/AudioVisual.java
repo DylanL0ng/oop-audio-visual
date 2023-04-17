1068f6865a8faf14f5891d6163c26d9e0b36f551
CallumOB <callumobrien2004@gmail.com>
2023-04-17 20:01:19 +0100

    Callum: Added Visual and VisualException classes as requested.

--- java/src/ie/tudublin/AudioVisual.java
+++ java/src/ie/tudublin/AudioVisual.java
@@ -1,16 +1,8 @@
 package ie.tudublin;
 
-import ddf.minim.*;
-import processing.core.PApplet;
-
-public class AudioVisual extends PApplet {
+public class AudioVisual extends Visual {
 
     Audio1 example;
-    Minim minim;
-
-    // All of these classes require imports from the minim library.
-    AudioPlayer ap; // Plays audio
-    AudioBuffer ab; // A buffer containing all of the audio samples.
 
     public void settings() {
         size(1024, 800);
@@ -18,15 +10,11 @@ public class AudioVisual extends PApplet {
 
     public void setup() {
 
-        int frameSize = 1024;
-        
         colorMode(HSB);
         background(0);
-        
-        minim = new Minim(this); // 'this' is a reference to the current applet. Equivalent to 'self' in Python.
-        ap = minim.loadFile("java/data/aria_math.mp3", frameSize);
-        ab = ap.mix; // Audio buffer will contain samples from a mix of the left and right channels
-        // of the file.
+
+        startMinim();
+        loadAudio("java/data/aria_math.mp3");
         
         smooth();
         /*
